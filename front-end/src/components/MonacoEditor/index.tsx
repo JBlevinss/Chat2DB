8a4e2ecde2533e116c6e3c4a576630b567ffe65c
Jerry Fan <huanyueyaoqin@qq.com>
2023-06-11 22:38:50 +0800

    feat: syntax-parser

--- front-end/src/components/MonacoEditor/index.tsx
+++ front-end/src/components/MonacoEditor/index.tsx
@@ -2,9 +2,12 @@ import React, { useEffect, useRef, useState } from 'react';
 import classnames from 'classnames';
 import { useTheme } from '@/hooks';
 import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
+import { language } from 'monaco-editor/esm/vs/basic-languages/sql/sql';
+const { keywords: SQLKeys } = language;
 import { editorDefaultOptions } from '@/constants/monacoEditor';
 import styles from './index.less';
 import { ThemeType } from '@/constants/common';
+import { monacoSqlAutocomplete } from './syntax-parser/plugin/monaco-plugin';
 
 export type IEditorIns = monaco.editor.IStandaloneCodeEditor;
 export type IEditorOptions = monaco.editor.IStandaloneEditorConstructionOptions;
@@ -37,6 +40,8 @@ function MonacoEditor(props: IProps) {
   // 受控暂存value
   const valRef = useRef<string>('');
 
+  // const;
+
   const [appTheme] = useTheme();
 
   // init
@@ -47,7 +52,7 @@ function MonacoEditor(props: IProps) {
         ...editorDefaultOptions,
         ...options,
         value,
-        language,
+        language: 'sql',
         theme:
           appTheme.backgroundColor === ThemeType.Light
             ? 'default'
@@ -67,6 +72,9 @@ function MonacoEditor(props: IProps) {
         'editor.background': '#22272e', //背景色
       },
     });
+
+    // monacoSqlAutocomplete(monaco, editorIns);
+    handleRegisterTigger();
     return () => {
       if (props.needDestroy) editorRef.current && editorRef.current.dispose();
     };
@@ -118,6 +126,75 @@ function MonacoEditor(props: IProps) {
     );
   }, [appTheme.backgroundColor]);
 
+  const handleRegisterTigger = () => {
+    // SQL关键词、 数据库、 表 、列
+
+    const hintData = { table1: 'table1', table2: 'table2' };
+    // 获取 SQL 语法提示
+    const getSQLSuggest = () => {
+      return SQLKeys.map((key: any) => ({
+        label: key,
+        kind: monaco.languages.CompletionItemKind.Keyword,
+        insertText: key,
+        detail: '<Keywords>',
+      }));
+    };
+
+    // 获取一级数据
+    const getFirstSuggest = () => {
+      return Object.keys(hintData).map((key) => ({
+        label: key,
+        kind: monaco.languages.CompletionItemKind.Method,
+        insertText: key,
+        detail: '<Database>',
+      }));
+    };
+
+    // 获取二级数据
+    // const getSecondSuggest = (keys: string) => {
+    //   const secondNames = hintData[keys];
+    //   if (!secondNames) {
+    //     return [];
+    //   }
+    //   return (secondNames || []).map((name: any) => ({
+    //     label: name,
+    //     kind: monaco.languages.CompletionItemKind.Snippet,
+    //     insertText: name,
+    //     detail: '<Table>',
+    //   }));
+    // };
+    monaco.languages.registerCompletionItemProvider('sql', {
+      triggerCharacters: [' ', ...SQLKeys],
+      provideCompletionItems: (
+        model: monaco.editor.ITextModel,
+        position: monaco.Position,
+      ) => {
+        let suggestions: any = [];
+        const { lineNumber, column } = position;
+        const textBeforePointer = model.getValueInRange({
+          startLineNumber: lineNumber,
+          startColumn: 0,
+          endLineNumber: lineNumber,
+          endColumn: column,
+        });
+        const tokens = textBeforePointer.trim().split(/\s+/);
+        const lastToken = tokens[tokens.length - 1]; // 获取最后一段非空字符串
+
+        if (lastToken.endsWith('.')) {
+          const tokenNoDot = lastToken.slice(0, lastToken.length - 1);
+          // suggestions = [...getSecondSuggest(tokenNoDot)];
+          suggestions = [];
+        } else if (lastToken === '.') {
+          suggestions = [];
+        } else {
+          suggestions = [...getFirstSuggest(), ...getSQLSuggest()];
+        }
+        return {
+          suggestions,
+        };
+      },
+    });
+  };
   return (
     <div className={classnames(className, styles.box)}>
       <div id={`monaco-editor-${id}`} className={styles.editorContainer} />
